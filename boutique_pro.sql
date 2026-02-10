5f78d62e1dfc058a621b0057753778d98fb1f972
bineta66 <binetabadadiane6@gmail.com>
2026-02-10 11:16:04 +0000

    Ajout du dump de la base de données

---
+++ boutique_pro.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
+--
+-- Host: localhost    Database: boutique_pro
+-- ------------------------------------------------------
+-- Server version	8.0.45-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_categorie` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Informatique'),(2,'Informatique');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historique`
+--
+
+DROP TABLE IF EXISTS `historique`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historique` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idproduit` int NOT NULL,
+  `type_mouvement` enum('ENTREE','SORTIE') DEFAULT NULL,
+  `quantite` int DEFAULT NULL,
+  `stock_actuel` int DEFAULT NULL,
+  `en_rupture` tinyint(1) DEFAULT NULL,
+  `date_mouvement` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historique`
+--
+
+LOCK TABLES `historique` WRITE;
+/*!40000 ALTER TABLE `historique` DISABLE KEYS */;
+INSERT INTO `historique` VALUES (1,1,'ENTREE',27,54,0,'2026-02-09 22:26:26');
+/*!40000 ALTER TABLE `historique` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mouvements`
+--
+
+DROP TABLE IF EXISTS `mouvements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mouvements` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idproduit` int NOT NULL,
+  `type_mouvement` enum('ENTREE','SORTIE') NOT NULL,
+  `quantite` int NOT NULL,
+  `date_mouvement` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idproduit` (`idproduit`),
+  CONSTRAINT `mouvements_ibfk_1` FOREIGN KEY (`idproduit`) REFERENCES `produits` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mouvements`
+--
+
+LOCK TABLES `mouvements` WRITE;
+/*!40000 ALTER TABLE `mouvements` DISABLE KEYS */;
+INSERT INTO `mouvements` VALUES (1,1,'ENTREE',27,'2026-02-09 22:26:26');
+/*!40000 ALTER TABLE `mouvements` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `after_mouvement_insert` AFTER INSERT ON `mouvements` FOR EACH ROW BEGIN
+    
+    IF NEW.type_mouvement = 'ENTREE' THEN
+        UPDATE produits
+        SET stock = stock + NEW.quantite
+        WHERE id = NEW.idproduit;
+    ELSEIF NEW.type_mouvement = 'SORTIE' THEN
+        UPDATE produits
+        SET stock = stock - NEW.quantite
+        WHERE id = NEW.idproduit;
+    END IF;
+
+    
+    UPDATE produits
+    SET en_rupture = IF(stock < 5, TRUE, FALSE)
+    WHERE id = NEW.idproduit;
+
+    
+    INSERT INTO historique(idproduit, type_mouvement, quantite, date_mouvement, stock_actuel, en_rupture)
+    SELECT NEW.idproduit, NEW.type_mouvement, NEW.quantite, NEW.date_mouvement, stock, en_rupture
+    FROM produits
+    WHERE id = NEW.idproduit;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `produits`
+--
+
+DROP TABLE IF EXISTS `produits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produits` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `designation` varchar(150) NOT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL DEFAULT '0',
+  `idcategorie` int NOT NULL,
+  `en_rupture` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idcategorie` (`idcategorie`),
+  CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`idcategorie`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produits`
+--
+
+LOCK TABLES `produits` WRITE;
+/*!40000 ALTER TABLE `produits` DISABLE KEYS */;
+INSERT INTO `produits` VALUES (1,'ordinateur',2300000.00,54,1,0);
+/*!40000 ALTER TABLE `produits` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-10 11:14:05
